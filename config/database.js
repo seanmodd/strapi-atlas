74672929e34cce9dd60c21e615ec3d8b03f7e6c9
Sean <sean@vestaridefair.com>
2021-09-06 23:16:37 -0700

    nice

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'senpexcluster.dn1ks.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-atlas'),
+        username: env('DATABASE_USERNAME', 'seanmodd'),
+        password: env('DATABASE_PASSWORD', '2thepack'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7346c94-4f8b-4a72-b654-a1a950861e07'
+};
