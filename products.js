5861efa0ce648563219e7c0a8d2a940351a255a1
Sean <sean@vestaridefair.com>
2021-09-07 10:18:26 -0700

    updating the new strapi atlas store with the proper data and logic for authentication

---
+++ products.js
@@ -0,0 +1,192 @@
+export const products = [
+  {
+    'status': 'published',
+    '_id': '613795db1755fffee2a249e4',
+    'title': 'Pink Hoodie',
+    'slug': 'pink-hoodie',
+    'description': 'This Heavyweight Recycled Cotton Hoodie is made with a responsibly sourced recycled and organic cotton mix. The Black color is created using a recycled water system. The fabric weight is a thicker, heavyweight recycled cotton that’s perfect for days you want a cozier feel. The material is brushed on the inside with a soft, fuzzy, plush handfeel.',
+    'price': 99.99,
+    'Custom_field': [],
+    'createdAt': '2021-09-07T16:39:55.672Z',
+    'updatedAt': '2021-09-07T16:40:04.313Z',
+    '__v': 0,
+    'image': {
+      '_id': '613795d71755fffee2a249e3',
+      'name': 'Rose Pink Hoodie.jpg',
+      'alternativeText': '',
+      'caption': '',
+      'hash': 'Rose_Pink_Hoodie_bcc6c49f1c',
+      'ext': '.jpg',
+      'mime': 'image/jpeg',
+      'size': 45.93,
+      'width': 1080,
+      'height': 1440,
+      'url': '/uploads/Rose_Pink_Hoodie_bcc6c49f1c.jpg',
+      'formats': {
+        'thumbnail': {
+          'name': 'thumbnail_Rose Pink Hoodie.jpg',
+          'hash': 'thumbnail_Rose_Pink_Hoodie_bcc6c49f1c',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 117,
+          'height': 156,
+          'size': 1.92,
+          'path': null,
+          'url': '/uploads/thumbnail_Rose_Pink_Hoodie_bcc6c49f1c.jpg'
+        },
+        'large': {
+          'name': 'large_Rose Pink Hoodie.jpg',
+          'hash': 'large_Rose_Pink_Hoodie_bcc6c49f1c',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 750,
+          'height': 1000,
+          'size': 24.96,
+          'path': null,
+          'url': '/uploads/large_Rose_Pink_Hoodie_bcc6c49f1c.jpg'
+        },
+        'medium': {
+          'name': 'medium_Rose Pink Hoodie.jpg',
+          'hash': 'medium_Rose_Pink_Hoodie_bcc6c49f1c',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 563,
+          'height': 750,
+          'size': 15.64,
+          'path': null,
+          'url': '/uploads/medium_Rose_Pink_Hoodie_bcc6c49f1c.jpg'
+        },
+        'small': {
+          'name': 'small_Rose Pink Hoodie.jpg',
+          'hash': 'small_Rose_Pink_Hoodie_bcc6c49f1c',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 375,
+          'height': 500,
+          'size': 8.44,
+          'path': null,
+          'url': '/uploads/small_Rose_Pink_Hoodie_bcc6c49f1c.jpg'
+        }
+      },
+      'provider': 'local',
+      'related': [
+        '613795db1755fffee2a249e4'
+      ],
+      'createdAt': '2021-09-07T16:39:51.719Z',
+      'updatedAt': '2021-09-07T16:39:55.851Z',
+      '__v': 0,
+      'id': '613795d71755fffee2a249e3'
+    },
+    'categories': [
+      {
+        'products': [
+          '613795db1755fffee2a249e4',
+          '613796231755fffee2a249e7'
+        ],
+        '_id': '613703420953a6452b941630',
+        'name': 'Other',
+        'slug': 'other',
+        'createdAt': '2021-09-07T06:14:26.771Z',
+        'updatedAt': '2021-09-07T16:42:25.014Z',
+        '__v': 0,
+        'id': '613703420953a6452b941630'
+      }
+    ],
+    'id': '613795db1755fffee2a249e4'
+  },
+  {
+    'status': 'published',
+    '_id': '613796231755fffee2a249e7',
+    'slug': 'all-black-hoodie',
+    'price': 99.99,
+    'description': 'PANGAIA 365, our oh-so-soft essentials designed to be worn across the 4 seasons, 365 days a year.  The 365 hoodie is made from 100% Organic Cotton. It’s a medium weight fabric, with a loop back texture inside and a finishing that provides extra softness.  The Black color was created using a recycled water system.',
+    'title': 'All Black Hoodie',
+    'Custom_field': [],
+    'createdAt': '2021-09-07T16:41:07.328Z',
+    'updatedAt': '2021-09-07T16:42:25.118Z',
+    '__v': 0,
+    'image': {
+      '_id': '6137961d1755fffee2a249e6',
+      'name': 'Black 365 Heavyweight Hoodie.jpg',
+      'alternativeText': '',
+      'caption': '',
+      'hash': 'Black_365_Heavyweight_Hoodie_843f8c21e3',
+      'ext': '.jpg',
+      'mime': 'image/jpeg',
+      'size': 50.17,
+      'width': 1080,
+      'height': 1440,
+      'url': '/uploads/Black_365_Heavyweight_Hoodie_843f8c21e3.jpg',
+      'formats': {
+        'thumbnail': {
+          'name': 'thumbnail_Black 365 Heavyweight Hoodie.jpg',
+          'hash': 'thumbnail_Black_365_Heavyweight_Hoodie_843f8c21e3',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 117,
+          'height': 156,
+          'size': 1.97,
+          'path': null,
+          'url': '/uploads/thumbnail_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg'
+        },
+        'large': {
+          'name': 'large_Black 365 Heavyweight Hoodie.jpg',
+          'hash': 'large_Black_365_Heavyweight_Hoodie_843f8c21e3',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 750,
+          'height': 1000,
+          'size': 25.46,
+          'path': null,
+          'url': '/uploads/large_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg'
+        },
+        'medium': {
+          'name': 'medium_Black 365 Heavyweight Hoodie.jpg',
+          'hash': 'medium_Black_365_Heavyweight_Hoodie_843f8c21e3',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 563,
+          'height': 750,
+          'size': 15.15,
+          'path': null,
+          'url': '/uploads/medium_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg'
+        },
+        'small': {
+          'name': 'small_Black 365 Heavyweight Hoodie.jpg',
+          'hash': 'small_Black_365_Heavyweight_Hoodie_843f8c21e3',
+          'ext': '.jpg',
+          'mime': 'image/jpeg',
+          'width': 375,
+          'height': 500,
+          'size': 7.55,
+          'path': null,
+          'url': '/uploads/small_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg'
+        }
+      },
+      'provider': 'local',
+      'related': [
+        '613796231755fffee2a249e7'
+      ],
+      'createdAt': '2021-09-07T16:41:01.622Z',
+      'updatedAt': '2021-09-07T16:41:07.499Z',
+      '__v': 0,
+      'id': '6137961d1755fffee2a249e6'
+    },
+    'categories': [
+      {
+        'products': [
+          '613795db1755fffee2a249e4',
+          '613796231755fffee2a249e7'
+        ],
+        '_id': '613703420953a6452b941630',
+        'name': 'Other',
+        'slug': 'other',
+        'createdAt': '2021-09-07T06:14:26.771Z',
+        'updatedAt': '2021-09-07T16:42:25.014Z',
+        '__v': 0,
+        'id': '613703420953a6452b941630'
+      }
+    ],
+    'id': '613796231755fffee2a249e7'
+  }
+];
