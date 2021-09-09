65b3761d9e2506c28d13529a9a17ee180a4ce213
Sean <sean@vestaridefair.com>
2021-09-08 18:51:56 -0700

    lost my todos but setting up the app for ecommerce

---
+++ myproducts.js
@@ -0,0 +1,346 @@
+[
+  {
+    "status": "published",
+    "_id": "613795db1755fffee2a249e4",
+    "title": "Pink Hoodie",
+    "slug": "pink-hoodie",
+    "description": "This Heavyweight Recycled Cotton Hoodie is made with a responsibly sourced recycled and organic cotton mix. The Black color is created using a recycled water system. The fabric weight is a thicker, heavyweight recycled cotton that’s perfect for days you want a cozier feel. The material is brushed on the inside with a soft, fuzzy, plush handfeel.",
+    "price": 99.99,
+    "Custom_field": [],
+    "createdAt": "2021-09-07T16:39:55.672Z",
+    "updatedAt": "2021-09-07T16:40:04.313Z",
+    "__v": 0,
+    "image": {
+      "_id": "613795d71755fffee2a249e3",
+      "name": "Rose Pink Hoodie.jpg",
+      "alternativeText": "",
+      "caption": "",
+      "hash": "Rose_Pink_Hoodie_bcc6c49f1c",
+      "ext": ".jpg",
+      "mime": "image/jpeg",
+      "size": 45.93,
+      "width": 1080,
+      "height": 1440,
+      "url": "/uploads/Rose_Pink_Hoodie_bcc6c49f1c.jpg",
+      "formats": {
+        "thumbnail": {
+          "name": "thumbnail_Rose Pink Hoodie.jpg",
+          "hash": "thumbnail_Rose_Pink_Hoodie_bcc6c49f1c",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 117,
+          "height": 156,
+          "size": 1.92,
+          "path": null,
+          "url": "/uploads/thumbnail_Rose_Pink_Hoodie_bcc6c49f1c.jpg"
+        },
+        "large": {
+          "name": "large_Rose Pink Hoodie.jpg",
+          "hash": "large_Rose_Pink_Hoodie_bcc6c49f1c",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 750,
+          "height": 1000,
+          "size": 24.96,
+          "path": null,
+          "url": "/uploads/large_Rose_Pink_Hoodie_bcc6c49f1c.jpg"
+        },
+        "medium": {
+          "name": "medium_Rose Pink Hoodie.jpg",
+          "hash": "medium_Rose_Pink_Hoodie_bcc6c49f1c",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 563,
+          "height": 750,
+          "size": 15.64,
+          "path": null,
+          "url": "/uploads/medium_Rose_Pink_Hoodie_bcc6c49f1c.jpg"
+        },
+        "small": {
+          "name": "small_Rose Pink Hoodie.jpg",
+          "hash": "small_Rose_Pink_Hoodie_bcc6c49f1c",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 375,
+          "height": 500,
+          "size": 8.44,
+          "path": null,
+          "url": "/uploads/small_Rose_Pink_Hoodie_bcc6c49f1c.jpg"
+        }
+      },
+      "provider": "local",
+      "related": [
+        "613795db1755fffee2a249e4"
+      ],
+      "createdAt": "2021-09-07T16:39:51.719Z",
+      "updatedAt": "2021-09-07T16:39:55.851Z",
+      "__v": 0,
+      "id": "613795d71755fffee2a249e3"
+    },
+    "categories": [
+      {
+        "products": [
+          "613795db1755fffee2a249e4",
+          "613796231755fffee2a249e7",
+          "61379de25dba420ed5ebbc1a",
+          "61379e2e5dba420ed5ebbc1d"
+        ],
+        "_id": "613703420953a6452b941630",
+        "name": "Sweatshirts",
+        "slug": "sweatshirts",
+        "createdAt": "2021-09-07T06:14:26.771Z",
+        "updatedAt": "2021-09-08T23:39:40.671Z",
+        "__v": 0,
+        "id": "613703420953a6452b941630"
+      }
+    ],
+    "id": "613795db1755fffee2a249e4"
+  },
+  {
+    "status": "published",
+    "_id": "613796231755fffee2a249e7",
+    "slug": "all-black-hoodie",
+    "price": 99.99,
+    "description": "PANGAIA 365, our oh-so-soft essentials designed to be worn across the 4 seasons, 365 days a year.  The 365 hoodie is made from 100% Organic Cotton. It’s a medium weight fabric, with a loop back texture inside and a finishing that provides extra softness.  The Black color was created using a recycled water system.",
+    "title": "All Black Hoodie",
+    "Custom_field": [],
+    "createdAt": "2021-09-07T16:41:07.328Z",
+    "updatedAt": "2021-09-07T16:42:25.118Z",
+    "__v": 0,
+    "image": {
+      "_id": "6137961d1755fffee2a249e6",
+      "name": "Black 365 Heavyweight Hoodie.jpg",
+      "alternativeText": "",
+      "caption": "",
+      "hash": "Black_365_Heavyweight_Hoodie_843f8c21e3",
+      "ext": ".jpg",
+      "mime": "image/jpeg",
+      "size": 50.17,
+      "width": 1080,
+      "height": 1440,
+      "url": "/uploads/Black_365_Heavyweight_Hoodie_843f8c21e3.jpg",
+      "formats": {
+        "thumbnail": {
+          "name": "thumbnail_Black 365 Heavyweight Hoodie.jpg",
+          "hash": "thumbnail_Black_365_Heavyweight_Hoodie_843f8c21e3",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 117,
+          "height": 156,
+          "size": 1.97,
+          "path": null,
+          "url": "/uploads/thumbnail_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg"
+        },
+        "large": {
+          "name": "large_Black 365 Heavyweight Hoodie.jpg",
+          "hash": "large_Black_365_Heavyweight_Hoodie_843f8c21e3",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 750,
+          "height": 1000,
+          "size": 25.46,
+          "path": null,
+          "url": "/uploads/large_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg"
+        },
+        "medium": {
+          "name": "medium_Black 365 Heavyweight Hoodie.jpg",
+          "hash": "medium_Black_365_Heavyweight_Hoodie_843f8c21e3",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 563,
+          "height": 750,
+          "size": 15.15,
+          "path": null,
+          "url": "/uploads/medium_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg"
+        },
+        "small": {
+          "name": "small_Black 365 Heavyweight Hoodie.jpg",
+          "hash": "small_Black_365_Heavyweight_Hoodie_843f8c21e3",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 375,
+          "height": 500,
+          "size": 7.55,
+          "path": null,
+          "url": "/uploads/small_Black_365_Heavyweight_Hoodie_843f8c21e3.jpg"
+        }
+      },
+      "provider": "local",
+      "related": [
+        "613796231755fffee2a249e7"
+      ],
+      "createdAt": "2021-09-07T16:41:01.622Z",
+      "updatedAt": "2021-09-07T16:41:07.499Z",
+      "__v": 0,
+      "id": "6137961d1755fffee2a249e6"
+    },
+    "categories": [
+      {
+        "products": [
+          "613795db1755fffee2a249e4",
+          "613796231755fffee2a249e7",
+          "61379de25dba420ed5ebbc1a",
+          "61379e2e5dba420ed5ebbc1d"
+        ],
+        "_id": "613703420953a6452b941630",
+        "name": "Sweatshirts",
+        "slug": "sweatshirts",
+        "createdAt": "2021-09-07T06:14:26.771Z",
+        "updatedAt": "2021-09-08T23:39:40.671Z",
+        "__v": 0,
+        "id": "613703420953a6452b941630"
+      }
+    ],
+    "id": "613796231755fffee2a249e7"
+  },
+  {
+    "status": "published",
+    "_id": "61379de25dba420ed5ebbc1a",
+    "slug": "nylon-hoodie",
+    "price": 79.99,
+    "description": "This Hoodie is made from a specially formulated nylon that breaks down in only a few years when disposed of in the proper environments. It has a lightweight, organic cotton jersey lining, an adjustable drawstring hem, and concealed, side seam zips and elasticated cuffs.  They have a lightweight, organic cotton jersey lining, which has been treated with PPRMINT™ oil for its anti-odor properties to help it stay fresher for longer—saving water, energy and time.",
+    "title": "Nylon Hoodie",
+    "Custom_field": [],
+    "createdAt": "2021-09-07T17:14:10.152Z",
+    "updatedAt": "2021-09-07T17:14:10.432Z",
+    "__v": 0,
+    "image": {
+      "_id": "61379dce5dba420ed5ebbc19",
+      "name": "Nylon-Hoodie-Apple-Red-1_540x.jpg",
+      "alternativeText": "",
+      "caption": "",
+      "hash": "Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9",
+      "ext": ".jpg",
+      "mime": "image/jpeg",
+      "size": 11.88,
+      "width": 540,
+      "height": 720,
+      "url": "/uploads/Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9.jpg",
+      "formats": {
+        "thumbnail": {
+          "name": "thumbnail_Nylon-Hoodie-Apple-Red-1_540x.jpg",
+          "hash": "thumbnail_Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 117,
+          "height": 156,
+          "size": 2.21,
+          "path": null,
+          "url": "/uploads/thumbnail_Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9.jpg"
+        },
+        "small": {
+          "name": "small_Nylon-Hoodie-Apple-Red-1_540x.jpg",
+          "hash": "small_Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 375,
+          "height": 500,
+          "size": 8.48,
+          "path": null,
+          "url": "/uploads/small_Nylon_Hoodie_Apple_Red_1_540x_c9ec387ec9.jpg"
+        }
+      },
+      "provider": "local",
+      "related": [
+        "61379de25dba420ed5ebbc1a"
+      ],
+      "createdAt": "2021-09-07T17:13:50.263Z",
+      "updatedAt": "2021-09-07T17:14:10.315Z",
+      "__v": 0,
+      "id": "61379dce5dba420ed5ebbc19"
+    },
+    "categories": [
+      {
+        "products": [
+          "613795db1755fffee2a249e4",
+          "613796231755fffee2a249e7",
+          "61379de25dba420ed5ebbc1a",
+          "61379e2e5dba420ed5ebbc1d"
+        ],
+        "_id": "613703420953a6452b941630",
+        "name": "Sweatshirts",
+        "slug": "sweatshirts",
+        "createdAt": "2021-09-07T06:14:26.771Z",
+        "updatedAt": "2021-09-08T23:39:40.671Z",
+        "__v": 0,
+        "id": "613703420953a6452b941630"
+      }
+    ],
+    "id": "61379de25dba420ed5ebbc1a"
+  },
+  {
+    "status": "published",
+    "_id": "61379e2e5dba420ed5ebbc1d",
+    "slug": "nylon-zipped-hoodie",
+    "price": 179.99,
+    "description": "This Zipped Hoodie is made from a specially formulated nylon that breaks down in only a few years when disposed of in the proper environments. It has a lightweight, organic cotton jersey lining, an adjustable drawstring hem, and concealed, side seam zips and elasticated cuffs.  They have a lightweight, organic cotton jersey lining, which has been treated with PPRMINT™ oil for its anti-odor properties to help it stay fresher for longer—saving water, energy and time.",
+    "title": "Nylon Zipped Hoodie",
+    "Custom_field": [],
+    "createdAt": "2021-09-07T17:15:26.811Z",
+    "updatedAt": "2021-09-07T17:15:27.067Z",
+    "__v": 0,
+    "image": {
+      "_id": "61379e275dba420ed5ebbc1c",
+      "name": "Nylon-Zipped-Hoodie-Orchid-Purple-1_540x.jpg",
+      "alternativeText": "",
+      "caption": "",
+      "hash": "Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce",
+      "ext": ".jpg",
+      "mime": "image/jpeg",
+      "size": 12.55,
+      "width": 540,
+      "height": 720,
+      "url": "/uploads/Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce.jpg",
+      "formats": {
+        "thumbnail": {
+          "name": "thumbnail_Nylon-Zipped-Hoodie-Orchid-Purple-1_540x.jpg",
+          "hash": "thumbnail_Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 117,
+          "height": 156,
+          "size": 1.82,
+          "path": null,
+          "url": "/uploads/thumbnail_Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce.jpg"
+        },
+        "small": {
+          "name": "small_Nylon-Zipped-Hoodie-Orchid-Purple-1_540x.jpg",
+          "hash": "small_Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce",
+          "ext": ".jpg",
+          "mime": "image/jpeg",
+          "width": 375,
+          "height": 500,
+          "size": 8.33,
+          "path": null,
+          "url": "/uploads/small_Nylon_Zipped_Hoodie_Orchid_Purple_1_540x_13edf506ce.jpg"
+        }
+      },
+      "provider": "local",
+      "related": [
+        "61379e2e5dba420ed5ebbc1d"
+      ],
+      "createdAt": "2021-09-07T17:15:19.982Z",
+      "updatedAt": "2021-09-07T17:15:26.968Z",
+      "__v": 0,
+      "id": "61379e275dba420ed5ebbc1c"
+    },
+    "categories": [
+      {
+        "products": [
+          "613795db1755fffee2a249e4",
+          "613796231755fffee2a249e7",
+          "61379de25dba420ed5ebbc1a",
+          "61379e2e5dba420ed5ebbc1d"
+        ],
+        "_id": "613703420953a6452b941630",
+        "name": "Sweatshirts",
+        "slug": "sweatshirts",
+        "createdAt": "2021-09-07T06:14:26.771Z",
+        "updatedAt": "2021-09-08T23:39:40.671Z",
+        "__v": 0,
+        "id": "613703420953a6452b941630"
+      }
+    ],
+    "id": "61379e2e5dba420ed5ebbc1d"
+  }
+]
